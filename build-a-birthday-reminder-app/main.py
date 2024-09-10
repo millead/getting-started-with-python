b6bc1e43b41bcc46eed36e59af34eabc91185b04
Apollo Miller <apollo.miller@yahoo.com>
2024-09-10 15:30:04 -0400

    Set up initial main.py for birthday app

--- build-a-birthday-reminder-app/main.py
+++ build-a-birthday-reminder-app/main.py
@@ -1,1 +1,10 @@
-user_input = input("Which birthday are you looking for? ")
+user_input = input("Which birthday are you looking for? ")\
+
+if user_input == "Randy":
+    print("Yeah, birthday is 8/4/2005.")
+elif user_input == "Mali":
+    print("Birthday is July 4, 2020.")
+elif user_input == "Ninji":
+    print("Birthday is July 4, 2021.")
+else: 
+    print("Can't a birthday for that person")
