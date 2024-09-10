a22246f1accaa64e67049d2a4a4fcde295df8a4f
Apollo Miller <apollo.miller@yahoo.com>
2024-09-10 15:48:56 -0400

    Basic pay calculator app set up

--- build-an-hours-worked-function/main.py
+++ build-an-hours-worked-function/main.py
@@ -1,7 +1,9 @@
-# Write function here
 
-# Worked 40 hours at $20 an hour
-print(calculate_pay(40,20))
+# Write function here
+def calculate_pay(hours_worked, pay_per_hour):
+    return(hours_worked * pay_per_hour)
+# Worked 40 hours at $32 an hour
+print(calculate_pay(40,32))
 # Worked 50 hours at $20 an hour
 print(calculate_pay(50,20))
 # Worked 40 hours at $12 an hour
