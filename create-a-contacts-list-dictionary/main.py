4e8919b9f5ada8e99436f0cc69ce1bbab23c3bdb
Apollo Miller <apollo.miller@yahoo.com>
2024-09-16 07:55:06 -0400

    Continued updating personal assistant app

--- create-a-contacts-list-dictionary/main.py
+++ create-a-contacts-list-dictionary/main.py
@@ -1,5 +1,19 @@
-people = ["Ann", "Chelsea", "Nichole", "Max"]
-title = ["Marketing Coordinator", "Software Developer", "Sales Representative", "Technical Writer"]
+class PersonalAssistant:
 
-company_org = {}
+  def __init__(self):
+    self.contacts = {
+        'Ann': 'Marketing Coordinator', 'Chelsea': 'Software Developer','Nichole': 'Sales Representative'
+    }
+    self.todos = []
 
+
+  def get_contact(self, name):
+    if name in self.contacts:
+        return self.contacts[name]
+    else:
+        return "No contact with that name!"
+
+
+# Code to test the class
+assistant = PersonalAssistant()
+print(assistant.get_contact("Chelsea"))
